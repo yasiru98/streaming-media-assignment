57384791c9b0388a2e8d46852af4f118548d889c
yasiru98 <yasiru043@gmail.com>
2020-02-03 16:36:50 -0500

    updated files

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const htmlHandler = require('./htmlResponses.js');
+const mediaHandler = require('./mediaResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const onRequest = (request, response) => {
+  console.log(request.url);
+  switch (request.url) {
+    case '/':
+      htmlHandler.getIndex(request, response);
+      break;
+    case '/party.mp4':
+      mediaHandler.getParty(request, response);
+      break;
+    default:
+      htmlHandler.getIndex(request, response);
+      break;
+  }
+};
+
+http.createServer(onRequest).listen(port);
+console.log(`Listening on 127.0.0.1: ${port}`);
